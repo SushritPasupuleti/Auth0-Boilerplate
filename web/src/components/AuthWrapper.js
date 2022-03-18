c4a2ed013f9c4b7d46c3cf87ab49fb7e35bcd80e
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2022-03-18 16:22:07 +0530

    feat: get access Tokens

--- web/src/components/AuthWrapper.js
+++ web/src/components/AuthWrapper.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
 
 const AuthWrapper = (props) => {
-    const { user, isAuthenticated, isLoading } = useAuth0();
+    const { user, isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0();
+    const [authToken, setAuthToken] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const token = await getAccessTokenSilently({
+                    // audience: process.env.REACT_APP_API, /// <-- This is the API endpoint
+                    // scope: '', /// <-- This is the scope for the API endpoint (optional and better to skip for most cases)
+                });
+                // console.log(token);
+                setAuthToken(token);/// Set this to Global State (redux) or localstorage.
+                /// check README.md for more info.
+            } catch (e) {
+                console.error(e);
+            }
+        })();
+    }, [getAccessTokenSilently]);
 
     if (isLoading) {
         return (
