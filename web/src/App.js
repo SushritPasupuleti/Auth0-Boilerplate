b580d9f12f56a59053d83aa9461b74c4c42d5a04
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2022-03-18 14:50:09 +0530

    feat: setup login button and fix env issues

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginButton from './components/LoginButton';
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
+				<div
+					style={{
+						display: 'flex',
+					}}
 				>
-					Learn React
-				</a>
+					<LoginButton
+						title="Click to Login"
+					/>
+				</div>
 			</header>
 		</div>
 	);
