bb21d86ed3c4b3c4050c04ea6f3c6d04d4ec192f
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2022-03-19 11:38:50 +0530

    fix: env dependencies

--- server/index.js
+++ server/index.js
@@ -4,18 +4,17 @@ const cors = require("cors");
 const helmet = require("helmet");
 const jwt = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
-const authConfig = require("./src/auth_config.json");
 
 const app = express();
 
 const port = process.env.API_PORT || 5000;
 const appPort = process.env.SERVER_PORT || 3000;
-const appOrigin = authConfig.appOrigin || `http://localhost:${appPort}`;
+const appOrigin = `http://localhost:${appPort}`;
 
 if (
-    !authConfig.domain ||
-    !authConfig.audience ||
-    authConfig.audience === "YOUR_API_IDENTIFIER"
+    !process.env.API_AUTH0_DOMAIN ||
+    !process.env.API_AUTH0_AUDIENCE ||
+    process.env.API_AUTH0_AUDIENCE === "YOUR_API_IDENTIFIER"
 ) {
     console.log(
         "Exiting: Please make sure that auth_config.json is in place and populated with valid domain and audience values"
@@ -33,11 +32,11 @@ const checkJwt = jwt({
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 5,
-        jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`,
+        jwksUri: `https://${process.env.API_AUTH0_DOMAIN}/.well-known/jwks.json`,
     }),
 
-    audience: authConfig.audience,
-    issuer: `https://${authConfig.domain}/`,
+    audience: process.env.API_AUTH0_AUDIENCE,
+    issuer: `https://${process.env.API_AUTH0_DOMAIN}/`,
     algorithms: ["RS256"],
 });
 
